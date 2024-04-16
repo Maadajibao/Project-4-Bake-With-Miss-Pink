75838c9d69eb6e2e850a07110ffcc6f139ec7c99
Maada Jibao <maada.jibao01@gmail.com>
2024-04-16 01:56:43 +0000

    Add a Post model

--- bakery/models.py
+++ bakery/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    new_recipe = models.SlugField(max_length=200, unique=True)
+    baker = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="pastry_posts"
+    )
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
--- miss_pink_bakes/settings.py
+++ miss_pink_bakes/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--e0ww0c*0hj$q#a)@!*o!!j+wm8izs#6sd8r+^+7zt&gr52+fu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-maadajibao-project4bake-qbeiltvkxhw.ws-eu110.gitpod.io'
+ALLOWED_HOSTS = ['8000-maadajibao-project4bake-qbeiltvkxhw.ws-eu110.gitpod.io',
                 '.herokuapp.com'
                 ]
 
@@ -90,6 +90,11 @@ WSGI_APPLICATION = 'miss_pink_bakes.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.gitpod.io",
+    "https://*.herokuapp.com"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
