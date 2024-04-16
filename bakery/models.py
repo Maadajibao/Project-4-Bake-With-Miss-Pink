b18f93046f6db2d9a7d00c5e5a543d21177d9855
Maada Jibao <maada.jibao01@gmail.com>
2024-04-16 03:11:19 +0000

    Create commet model

--- bakery/models.py
+++ bakery/models.py
@@ -5,12 +5,23 @@ STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
 
+# Post, class for post attributes
+
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     new_recipe = models.SlugField(max_length=200, unique=True)
-    baker = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="pastry_posts"
-    )
+    baker = models.ForeignKey(User, on_delete=models.CASCADE, related_name="pastry_posts")
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+
+
+# Comments, class for comment attributes
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+
